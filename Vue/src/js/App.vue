0635a1478ec6df1e579472c9e14d771d3a6fabda
RodrigoZimmermann <rodrigo.l.zimmermann.rz@gmail.com>
2020-07-11 19:09:15 -0300

    cadastroJogo

---
+++ Vue/src/js/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <Jogo>
+</div>
+</template>
+
+<script>
+import Vue from "vue";
+import Jogo from "./components/Jogo/Jogo";
+
+export default Vue.extend({
+  data() {
+    return {
+      contador: 1
+    };
+  },
+  methods : {
+      incrementar(){
+          this.contador = this.contador + 1
+      }
+  },
+  components: {
+      Jogo
+  }
+});
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
---
+++ Vue/src/js/components/Jogo/Jogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <FormJogo></FormJogo>
+      <ListaJogos></ListaJogos>
+    </div>
+</template>
+
+<script>
+import FormJogo from './FormJogo';
+import ListaJogos from './ListaJogos';
+
+export default {
+    components: {
+        FormJogo,
+        ListaJogos
+    }
+}
+</script>
+
+<style>
+
+</style>

6db5e6dc612cd46385b0d33735bced4f220e3f03
RodrigoZimmermann <rodrigo.l.zimmermann.rz@gmail.com>
2020-07-11 22:11:50 -0300

    ServeAndVue

---
+++ Vue/src/js/models/Jogo.js
@@ -0,0 +1,10 @@
+class Jogo {
+    constructor(id, titulo, tipo, preco, plataforma, quantidade) {
+        this.id = id;
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.plataforma = plataforma;
+        this.quantidade = quantidade;
+    }
+}
